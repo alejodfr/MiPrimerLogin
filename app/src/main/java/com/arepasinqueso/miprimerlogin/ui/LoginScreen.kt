08ed879703eae26c7c145364a1775697f470e35a
Alejo <alejodfr@gmail.com>
2026-08-06 13:01:30 -0500

    feat: conectar botón de login con Retrofit y manejar estados
    - scope.launch + try/catch para llamar a RetrofitInstance.api.login
    - Actualiza uiState a Loading, Success o Error según el resultado

--- app/src/main/java/com/arepasinqueso/miprimerlogin/ui/LoginScreen.kt
+++ app/src/main/java/com/arepasinqueso/miprimerlogin/ui/LoginScreen.kt
@@ -2,22 +2,28 @@ package com.arepasinqueso.miprimerlogin.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import com.arepasinqueso.miprimerlogin.data.LoginRequest
+import com.arepasinqueso.miprimerlogin.data.RetrofitInstance
+import kotlinx.coroutines.launch
 
 @Composable
 fun LoginScreen(){
     var uiState by remember { mutableStateOf<LoginUiState>(LoginUiState.Idle) }
     var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    val scope = rememberCoroutineScope()
 
     Column {
         OutlinedTextField(
@@ -34,6 +40,24 @@ fun LoginScreen(){
             // desactivar el auto completado del teclado
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
         )
+
+
+
+        Button(onClick = {
+            scope.launch {
+                uiState = LoginUiState.Loading
+
+                try {
+                    val response = RetrofitInstance.api.login(LoginRequest(username, password))
+                    uiState = LoginUiState.Success(response)
+                } catch (e: Exception) {
+                    uiState = LoginUiState.Error(e.message ?: "Error desconocido")
+                }
+
+            }
+        }) {
+            Text("Entrar")
+        }
     }
 }
 
