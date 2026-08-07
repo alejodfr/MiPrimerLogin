5fce2b81cbd554066c01f1ddceef0f0083795f6c
Alejo <alejodfr@gmail.com>
2026-08-06 23:18:22 -0500

    feat: Manuel Salami

--- app/src/main/java/com/arepasinqueso/miprimerlogin/ui/LoginScreen.kt
+++ app/src/main/java/com/arepasinqueso/miprimerlogin/ui/LoginScreen.kt
@@ -29,7 +29,7 @@ fun LoginScreen(){
         OutlinedTextField(
             value = username,
             onValueChange = { username = it },
-            label = { Text("Usuario") }
+            label = { Text("manuel salami") }
         )
 
         OutlinedTextField(
