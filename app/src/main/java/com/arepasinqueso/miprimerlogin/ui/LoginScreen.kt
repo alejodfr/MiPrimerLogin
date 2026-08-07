e06dd4cf4203aa0edab6f148ade08307efc6d617
Alejo <alejodfr@gmail.com>
2026-08-06 23:26:45 -0500

    fix: mayusculas en manuel salami

--- app/src/main/java/com/arepasinqueso/miprimerlogin/ui/LoginScreen.kt
+++ app/src/main/java/com/arepasinqueso/miprimerlogin/ui/LoginScreen.kt
@@ -29,7 +29,7 @@ fun LoginScreen(){
         OutlinedTextField(
             value = username,
             onValueChange = { username = it },
-            label = { Text("manuel salami") }
+            label = { Text("Manuel Salami") }
         )
 
         OutlinedTextField(
